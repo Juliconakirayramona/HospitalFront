0107d83a43bae0dd2e5c8da03119dba594b42aa9
Samuel Ortiz Bermudez <samuel.ortber@gmail.com>
2025-11-15 13:14:43 -0500

    medicos

---
+++ src/app/features/auth/medical-appointment.component/medical-appointment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medical-appointment.component',
+  imports: [],
+  templateUrl: './medical-appointment.component.html',
+  styleUrl: './medical-appointment.component.scss',
+})
+export class MedicalAppointmentComponent {
+
+}
